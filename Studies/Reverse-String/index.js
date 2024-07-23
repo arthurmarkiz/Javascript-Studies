bd9acf43a4ee7b6f4aa2f36b3d94d43d45669c88
Arthur Markiz <tutumarkiz26@gmail.com>
2024-07-23 16:11:47 -0300

    Create Reverse String Studie

---
+++ Studies/Reverse-String/index.js
@@ -0,0 +1,45 @@
+// 01. Reverse String with the Reverse Method (ez way)
+
+const reverse = (str = "") => {
+  if (str == "") return str;
+  const string = String(str);
+  return console.log(`${string} = ${string.split("").reverse().join("")}`);
+};
+
+console.log("Method 1");
+reverse(123);
+reverse("apple");
+console.log("--------");
+// ===================================================
+
+// 02. Reverse String with For of loop and temporary variable
+
+const reverse2 = (str = "") => {
+  const string = String(str);
+  let reversed = "";
+
+  for (let char of string) {
+    reversed = char + reversed;
+  }
+  return console.log(string + " = " + reversed);
+};
+
+console.log("Method 2");
+reverse2(123);
+reverse2("apple");
+console.log("--------");
+
+// ===================================================
+
+// 3. Reverse String with the Reduce Method
+
+const reverse3 = (str = "") => {
+  const string = String(str);
+  return console.log(
+    `${string} = ${string.split("").reduce((rev, char) => char + rev, "")}`
+  );
+};
+
+console.log("Method 3");
+reverse3(123);
+reverse3("apple");
